f1bea3a728b9d8db9e696f8c0de919e63f92e6ba
john katua <johnkatua99@gmail.com>
2022-06-16 18:58:49 +0300

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="footer--wrapper">Footer</footer>;
+  return (
+    <footer className="footer--wrapper">
+      &copy; 2022 John Katua. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
