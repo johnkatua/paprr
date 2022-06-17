83897c88b7ac87b47e866187cc3d01e63cea9613
john katua <johnkatua99@gmail.com>
2022-06-17 14:01:36 +0300

    filter paper by name

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { usePapersQuery } from "../services/paperApi";
 import Card from "../components/Card";
 
 const Home = () => {
   const { data } = usePapersQuery();
+  const [name, setName] = useState('');
+  const [results, setResults] = useState(data?.data);
+
+  useEffect(() => {
+    setResults(data?.data);
+  }, [data]);
+
+  const filter = (e) => {
+    const searchedWord = e.target.value;
+
+    if (searchedWord !== '') {
+      const response = data.data.filter((paper) => {
+        return paper.name.toLowerCase().includes(searchedWord.toLowerCase());
+      });
+      setResults(response);
+    } else {
+      setResults(data && data.data)
+    }
+    setName(searchedWord);
+  }
+
 
   return (
     <div className="home--container">
-      <input />
+      <input type="search" value={name} onChange={filter} placeholder="search paper" />
       <div className="home--container__data">
-        {data &&
+        {results && results.length > 0 ? (
+          results.map((paper) => (
+            <Card key={paper._id} academicYear={paper.academicYear} name={paper.name} status={paper.status} year={paper.year} file={paper.file} />
+          ))
+        ) : (
+          <h2>No papers in here currently!!!</h2>
+        )}
+        {/* {data &&
         data.data.map((paper) => {
           return (
             <Card
@@ -22,7 +50,7 @@ const Home = () => {
               file={paper.file}
             />
           );
-        })}
+        })} */}
       </div>
     </div>
   );
