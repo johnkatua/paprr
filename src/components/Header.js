0b50818822c8168ae360a03e5173945c3e21ae48
john katua <johnkatua99@gmail.com>
2022-06-28 12:54:12 +0300

    added login and register buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,14 @@ const Header = () => {
   return (
     <header className="header--wrapper">
       <h1 className="header--wrapper__title">TUK-repo</h1>
+      <div className="header--wrapper__auth">
+        <button>
+          Register
+        </button>
+        <button>
+          Login
+        </button>
+      </div>
     </header>
   );
 };
