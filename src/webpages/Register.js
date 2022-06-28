97e175432d89abe9fb5065e59f495fd111528831
john katua <johnkatua99@gmail.com>
2022-06-28 14:36:32 +0300

    added inputs to the register page

--- src/webpages/Register.js
+++ src/webpages/Register.js
@@ -2,7 +2,25 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="register--wrapper">Register</div>
+    <div className="register--wrapper">
+      <div className="register--container">
+        <form>
+          <div className="register--container__details">
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" autoComplete="name" />
+          </div>
+          <div className="register--container__details">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" autoComplete="email" />
+          </div>
+          <div className="register--container__details">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" autoComplete="on" />
+          </div>
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
