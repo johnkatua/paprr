cbdb90a7638f0acecfec18bbe77d4b176a8e056a
john katua <johnkatua99@gmail.com>
2022-06-28 14:02:24 +0300

    added styling for my registration container

--- src/webpages/Register.js
+++ src/webpages/Register.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register--wrapper">Register</div>
+  );
 };
 
 export default Register;
