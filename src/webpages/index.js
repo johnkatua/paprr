1f8d02b0131437f6f0fb6a0ea3650305aebe9bad
john katua <johnkatua99@gmail.com>
2022-06-28 13:22:45 +0300

    added a wrapper for layout routes

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -10,20 +10,30 @@ import "./index.css";
 import Login from "./Login";
 import Register from "./Register";
 
+const LayoutRoute = ({ children }) => {
+  return <Layout>{children}</Layout>
+};
+
 const Webpages = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
-        <Layout>
+        {/* <Layout> */}
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/fsst" element={<MyBooks />} />
-            <Route path="/febe" element={<Favourites />} />
-            <Route path="/paper/:id" element={<FilteredPapers />} />
+            {/* <Route path="/fsst" element={<MyBooks />} />
+            <Route path="/febe" element={<Favourites />} /> */}
+            {/* <Route path="/paper/:id" element={<FilteredPapers />} /> */}
+            <Route path="/" element={<LayoutRoute>
+              <Home />
+            </LayoutRoute>} exact />
+            <Route path="paper/:id" element={<LayoutRoute>
+              <FilteredPapers />
+            </LayoutRoute>} />
           </Routes>
-        </Layout>
+        {/* </Layout> */}
       </Router>
     </Suspense>
   );
