77fa9ad0ff743f6f76db3a73e23c8e2c987e835e
john katua <johnkatua99@gmail.com>
2022-06-28 15:20:01 +0300

    added a login page

--- src/webpages/Login.js
+++ src/webpages/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="register--wrapper">
+      <div className="login--container">
+        <form>
+          <div className="register--container__details">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" autoComplete="email" />
+          </div>
+          <div className="register--container__details">
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="password" autoComplete="on" />
+          </div>
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
