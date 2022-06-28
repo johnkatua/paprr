2be36d14882f710b499e7af9138f5045be60949d
john katua <johnkatua99@gmail.com>
2022-06-28 13:25:37 +0300

    deleted all commented code

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "../components/Layout";
 import Home from "./home";
-import MyBooks from "./mybooks";
-import Favourites from "./favourites";
 import FilteredPapers from "./fiilteredPapers";
 import "./index.css";
 import Login from "./Login";
@@ -18,14 +16,9 @@ const Webpages = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
-        {/* <Layout> */}
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            {/* <Route path="/fsst" element={<MyBooks />} />
-            <Route path="/febe" element={<Favourites />} /> */}
-            {/* <Route path="/paper/:id" element={<FilteredPapers />} /> */}
             <Route path="/" element={<LayoutRoute>
               <Home />
             </LayoutRoute>} exact />
@@ -33,7 +26,6 @@ const Webpages = () => {
               <FilteredPapers />
             </LayoutRoute>} />
           </Routes>
-        {/* </Layout> */}
       </Router>
     </Suspense>
   );
